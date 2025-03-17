7cdb0a85ed4911174b8748bed37f6a4d76579114
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-17 01:40:20 +0000

    cambios actualizados

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,10 +5,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-    const [ color, setColor] = useState("");
+    const [ color, setColor] = useState("danger");
  
      return (
-        <div className="card d-flex justify-content-center bg-dark mt-5 mx-auto p-5 " style={{ maxWidth: "250px" }}>
+        <div className="card d-flex justify-content-center bg-dark mt-5 mx-auto p-5 " style={{ maxWidth: "150px" }}>
              <div className = "trafic-light d-flex flex-column align-items-center">
                 
                 <button onClick={(event) => {setColor("danger")}}className={`btn btn-danger rounded-circle mx-2 ${color === "danger" ? "brillo" : ""}`} style={{ width: "80px", height: "80px" }}></button>    
