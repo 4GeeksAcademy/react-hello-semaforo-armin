b1d79882198850e2cffac698992c75c82e8df0d9
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-17 01:53:17 +0000

    cambios actualizados

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -8,12 +8,13 @@ const Home = () => {
     const [ color, setColor] = useState("danger");
  
      return (
+        
         <div className="card d-flex justify-content-center bg-dark mt-5 mx-auto p-5 " style={{ maxWidth: "150px" }}>
              <div className = "trafic-light d-flex flex-column align-items-center">
                 
-                <button onClick={(event) => {setColor("danger")}}className={`btn btn-danger rounded-circle mx-2 ${color === "danger" ? "brillo" : ""}`} style={{ width: "80px", height: "80px" }}></button>    
-                <button onClick={(event) => {setColor("warning")}}className={`btn btn-warning rounded-circle mx-2 ${color === "warning" ? "brillo" : ""}`} style={{ width: "80px", height: "80px" }}></button>    
-                <button onClick={(event) => {setColor("success")}}className={`btn btn-success rounded-circle mx-2 ${color === "success" ? "brillo" : ""}`} style={{ width: "80px", height: "80px" }}></button>    
+                <button onClick={() => {setColor("danger")}}className={`btn btn-danger rounded-circle mx-2 ${color === "danger" ? "brillo" : ""}`} style={{ width: "80px", height: "80px" }}></button>    
+                <button onClick={() => {setColor("warning")}}className={`btn btn-warning rounded-circle mx-2 ${color === "warning" ? "brillo" : ""}`} style={{ width: "80px", height: "80px" }}></button>    
+                <button onClick={() => {setColor("success")}}className={`btn btn-success rounded-circle mx-2 ${color === "success" ? "brillo" : ""}`} style={{ width: "80px", height: "80px" }}></button>    
              </div>
          </div>
      );
